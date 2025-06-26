08c5c10d8e014091b89ce498a769d05a689609ff
Carlo Valdivia Luna <cvaldivialu@unsa.edu.pe>
2025-06-26 07:56:20 -0500

    feat: update package.json and package-lock.json to include Swagger and validation libraries; enhance main.ts for Swagger setup and CORS; modify AppModule to import DistrictsModule; refactor PrismaService import path; remove unused AppController tests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from '../modules/users/users.module';
+import { DistrictsModule } from '../modules/districts/districts.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, DistrictsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+  }));
+
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS Prisma Example')
+    .setDescription('API documentation for NestJS with Prisma')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
+  app.enableCors();
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
