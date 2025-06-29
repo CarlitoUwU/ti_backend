5dcc167b3cb7ceaa095317c821d99fb54e43ed74
Carlo Valdivia Luna <cvaldivialu@unsa.edu.pe>
2025-06-29 13:42:15 -0500

    feat(mails): add mailer module and service for password recovery
    - Added @nestjs-modules/mailer and nodemailer dependencies.
    - Created MailsModule to configure mailer with Handlebars template engine.
    - Implemented MailsService with a method to send password reset emails.
    - Added Handlebars template for password recovery email.

---
+++ src/modules/mails/mails.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { MailerService } from '@nestjs-modules/mailer';
+
+@Injectable()
+export class MailsService {
+  constructor(private mailerService: MailerService) { }
+
+  async sendUserResetPassword(username: string, email: string, code:number) {
+
+    await this.mailerService.sendMail({
+      to: email,
+      subject: 'Recuperación de cuenta',
+      template: './reset-password',
+      context: {
+        username,
+        email,
+        code,
+      }
+    })
+  }
+}
